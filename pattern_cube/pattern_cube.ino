5be39a9009c868a57187a8fc2c81dc0f7d4ef7a1
Jasper Wang <18850544459@163.com>
2019-10-02 15:03:28 -0400

    add led cube and esp32

---
+++ pattern_cube/pattern_cube.ino
@@ -0,0 +1,58 @@
+#include <avr/pgmspace.h>
+const uint8_t cubePattern[] PROGMEM = {0b00000001, 0b00010110, 0b01101000, 0b10000000, 0b11111111, 0b10101010};
+const int size = sizeof(cubePattern);
+
+void setup() {
+//  Serial.begin(9600);
+  digitalWrite(11, LOW);  //we change pinMode on cathod to control current
+  digitalWrite(12, LOW);
+
+  pinMode(2, OUTPUT); //we use High and Low for common cathod
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+//  Serial.println(size);
+  for (uint8_t i = 0; i < size; i++) {
+    displayByte(pgm_read_byte(cubePattern+i), 200);
+  }
+}
+
+void displayByte(uint8_t data, uint16_t duration) {
+  unsigned long beginTime = millis();
+  while ((millis() - beginTime) < duration) {
+    enableLayer(0);
+    setLayerOutput(bitRead(data, 0), bitRead(data, 1), bitRead(data, 2), bitRead(data, 3));
+    delay(8);
+    disableLayers();
+    enableLayer(1);
+    setLayerOutput(bitRead(data, 4), bitRead(data, 5), bitRead(data, 6), bitRead(data, 7));
+    delay(8);
+    disableLayers();
+  };
+}
+
+void setLayerOutput(boolean led0, boolean led1, boolean led2, boolean led3) {
+  digitalWrite(2, led0);
+  digitalWrite(3, led1);
+  digitalWrite(4, led2);
+  digitalWrite(5, led3);
+}
+
+void enableLayer(int layer) {
+  if (layer == 0) {
+    pinMode(12, INPUT);
+    pinMode(11, OUTPUT);
+  } else if (layer == 1) {
+    pinMode(11, INPUT);
+    pinMode(12, OUTPUT);
+  } else {
+    disableLayers();
+  }
+}
+void disableLayers() {
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+}
